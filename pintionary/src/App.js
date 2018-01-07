9d177353f6b4e2deeb89f5f0c27a29ce4065c3d0
Justin Licari <licarijd@mcmaster.ca>
2018-01-06 21:51:48 -0500

    map.html content added to react app index, with quick tips overlay

--- pintionary/src/App.js
+++ pintionary/src/App.js
@@ -5,14 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div  className="dimmed"></div>
+        <body>
+          <h1 className="App-intro">Quick tips to familiarize you with Pintionary</h1>
+          <button className="App-intro-button">
+          Okay, let's start
+        </button >
+        </body>       
       </div>
     );
   }
