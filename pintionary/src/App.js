092e425ca5e6c2e6606968df6bf4e60783134d49
Justin Licari <licarijd@mcmaster.ca>
2018-01-07 03:26:53 -0500

    Google Maps API components: search location and pin functionality. Also added intro overlay with dismiss button

--- pintionary/src/App.js
+++ pintionary/src/App.js
@@ -3,16 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  //Dismiss overlay when user clicks "get started" button
+  dismissIntro(){
+    var introElement = document.getElementById('intro');
+    introElement.hidden = true;
+  }
+    
+  //Render introduction overlay when web app starts
   render() {
     return (
-      <div>
+      <div id = "intro">
         <img src={logo} className="App-logo" alt="logo" />
         <div  className="dimmed"></div>
         <body>
           <h1 className="App-intro">Quick tips to familiarize you with Pintionary</h1>
-          <button className="App-intro-button">
+          <button  onClick={this.dismissIntro} className="App-intro-button">
           Okay, let's start
-        </button >
+        </button>
         </body>       
       </div>
     );
